aaad39ab5e6c4e1f5d64ffb684c3a28adc11fa7e
zed <zainebb7k9h1@gmail.com>
2023-10-09 13:55:44 +0100

    **-

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello world")
+require('dotenv').config();
+const Discord = require('discord.js')
+
+const client = new Discord.Client({ intents: [Discord.IntentsBitField.Flags.MessageContent, Discord.IntentsBitField.Flags.GuildMessages, Discord.IntentsBitField.Flags.Guilds]})
+
+client.once("ready", () => {
+    console.log("connected")
+})
+
+client.login(process.env.BOT_TOKEN)
