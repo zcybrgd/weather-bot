eef523933ff583a2f4cc1197fd271f7e162bf1b8
zed <zainebb7k9h1@gmail.com>
2023-10-16 22:28:42 +0100

    -- trying basic random functionalities to get familiar

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 require('dotenv').config();
 const Discord = require('discord.js')
+const axios = require('axios')
 
 const client = new Discord.Client({ intents: [Discord.IntentsBitField.Flags.Guilds]})
+const PREFIX = '!'
 
 client.once(Discord.Events.ClientReady, () => {
-    console.log("connected")
+    console.log(`Logged in as ${client.user.tag}`);
+})
+
+
+client.on('message', async (m) =>{
+    if(m.author.bot) return;
+
+    const args = m.content.slice(PREFIX.length).trim().split(/ +/)
+    const command = args.shift().toLowerCase()
+
+    if(command === 'weather') {
+        const location = args.join(' ')
+        if(!location){
+            m.channel.send('Please specify a location so we can provide the weather information')
+            return;
+        }
+    }
+
+    try {
+        const response = await axios.get( `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${process.env.OPENWEATHER_KEY}`)
+        const weatherData = response.data
+        console.log('weather : ' + weatherData)
+        const temperature = weatherData.main.temp
+        const description = weatherData.weather[0].description
+
+        m.channel.send(`Weather in ${location}: ${description}, Temperature: ${temperature}°C`)
+    } catch(e) {
+        console.error(e)
+        m.channel.send('Please Try Again')
+    }
 })
 
 client.login(process.env.BOT_TOKEN)
