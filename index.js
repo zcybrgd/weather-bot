6801930d99af12ab8b898c8655ce55b655fb8bef
zed <zainebb7k9h1@gmail.com>
2023-10-21 00:26:40 +0100

    config prob

--- index.js
+++ index.js
@@ -1,41 +1,50 @@
 require('dotenv').config();
 const Discord = require('discord.js')
 const axios = require('axios')
-
-const client = new Discord.Client({ intents: [Discord.IntentsBitField.Flags.Guilds]})
+const client = new Discord.Client({
+    intents: [
+        Discord.IntentsBitField.Flags.GuildMessages, 
+        Discord.IntentsBitField.Flags.Guilds , 
+        Discord.IntentsBitField.Flags.GuildMembers , 
+        Discord.IntentsBitField.Flags.MessageContent],
+});
 const PREFIX = '!'
+
+
 
 client.once(Discord.Events.ClientReady, () => {
     console.log(`Logged in as ${client.user.tag}`);
 })
 
 
-client.on('message', async (m) =>{
-    if(m.author.bot) return;
+client.on('messageCreate', async (message) =>{
+    if(message.author.bot) return;
 
-    const args = m.content.slice(PREFIX.length).trim().split(/ +/)
+    const args = message.content.slice(PREFIX.length).trim().split(/ +/)
     const command = args.shift().toLowerCase()
+    const location = args.join(' ')
 
-    if(command === 'weather') {
-        const location = args.join(' ')
-        if(!location){
-            m.channel.send('Please specify a location so we can provide the weather information')
+    if (command === 'weather') {
+        if (!location) {
+            m.channel.send('Please specify a location so we can provide the weather information');
             return;
         }
+    
+        try {
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${process.env.OPENWEATHER_KEY}`);
+            const weatherData = response.data;
+            console.log('weather : ' + weatherData);
+            const temperature = weatherData.main.temp;
+            console.log('temp : ' + temperature);
+            const description = weatherData.weather[0].description;
+    
+            message.channel.send(`Weather in ${location}: ${description}, Temperature: ${temperature}°C`);
+        } catch (e) {
+            console.error(e);
+            message.channel.send('Unable to fetch weather data. Please try again later.');
+        }
     }
-
-    try {
-        const response = await axios.get( `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${process.env.OPENWEATHER_KEY}`)
-        const weatherData = response.data
-        console.log('weather : ' + weatherData)
-        const temperature = weatherData.main.temp
-        const description = weatherData.weather[0].description
-
-        m.channel.send(`Weather in ${location}: ${description}, Temperature: ${temperature}°C`)
-    } catch(e) {
-        console.error(e)
-        m.channel.send('Please Try Again')
-    }
+    
 })
 
 client.login(process.env.BOT_TOKEN)
